af8bea0a630aec2a860bb40baddfee94b6eec5cd
Tobias <42217856+caicedos@users.noreply.github.com>
2018-10-30 17:14:21 -0400

    update

--- liri.js
+++ liri.js
@@ -4,90 +4,107 @@ var request = require('request');
 var keys = require('./Keys');
 var Spotify = require('node-spotify-api');
 var moment = require('moment');
-
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
 
-// if (process.argv[2] === "concert-this") {
+if (process.argv[2] === "concert-this") {
 
-//     var artist = process.argv[3];
+  concertThis()
 
-//     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+} else if (process.argv[2] === "spotify-this-song") {
+  var query = process.argv[3];
+  spotifyThisSong(query)
 
-//         if (!error && response.statusCode === 200) {
+} else if (process.argv[2] === "movie-this") {
 
-//             JSON.parse(body).forEach(function (object) {
+  movieThis()
 
-//                 console.log("Name of the Venue:" + "" + object.venue.name);
-//                 console.log("------------------------------------");
-//                 console.log("Venue location:" + "" + object.venue.city);
-//                 console.log("------------------------------------");
-//                 console.log("Date of the event:" + "" + moment(object.datetime).format("MM/DD/YYYY"));
-//             });
-//         }
-//     });
+} else if (process.argv[2] === "do-what-it-says") {
 
-// } 
-// if (process.argv[2] === "spotify-this-song") {
+  fs.readFile("random.txt", "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
 
-//   var query = process.argv[3];
+    var output = data.split(",");
 
-//   spotify.search({
-//     type: "track",
-//     query: query,
-//     limit: 5
-//   }, function (err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
+    output.forEach(function (element) {
 
-//     data.tracks.items.forEach(function (object) {
+     console.log(JSON.parse(element))
 
-//       console.log("Artist:" + "" + object.artists[0].name);
-//       console.log("------------------------------------");
-//       console.log("Song:" + "" + object.song);
-//       console.log("------------------------------------");
-//       console.log("Preview:" + "" + object.preview_url);
-//       console.log("------------------------------------");
-//       console.log("Album:" + "" + object.album.name);
-//       console.log("------------------------------------");
+    });
+  })
+}
 
-//     });
-//   });
+function concertThis() {
+  var artist = process.argv[3];
+
+  request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+
+      JSON.parse(body).forEach(function (object) {
+
+        console.log("Name of the Venue:" + "" + object.venue.name);
+        console.log("------------------------------------");
+        console.log("Venue location:" + "" + object.venue.city);
+        console.log("------------------------------------");
+        console.log("Date of the event:" + "" + moment(object.datetime).format("MM/DD/YYYY"));
+      });
+    }
+  });
+}
+
+function spotifyThisSong() {
+
+  var query = process.argv[3];
+
+  spotify.search({
+    type: "track",
+    query: query,
+    limit: 5
+  }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    data.tracks.items.forEach(function (object) {
+
+      console.log("Artist:" + "" + object.artists[0].name);
+      console.log("------------------------------------");
+      console.log("Song:" + "" + object.song);
+      console.log("------------------------------------");
+      console.log("Preview:" + "" + object.preview_url);
+      console.log("------------------------------------");
+      console.log("Album:" + "" + object.album.name);
+      console.log("------------------------------------");
+
+    });
+  });
 
   //    * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-// }
+}
 
-// if (process.argv[2] === "movie-this") {
+function movieThis() {
 
-//   var movieName = process.argv[3];
+  var movieName = process.argv[3];
 
-//   request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-
-//     if (!error && response.statusCode === 200) {
-//       var pbody = JSON.parse(body)
-  
-//       console.log("Title: " + pbody.Title);
-//       console.log("Year: " + pbody.Year);
-//       console.log("IMBD Rating: " + pbody.imdbRating);
-//       console.log("Rotten Tomatoes Rating: " + pbody.Ratings[1].Value);
-//       console.log("Country: " + pbody.Country);
-//       console.log("Language: " + pbody.Language);
-//       console.log("Plot: " + pbody.Plot);
-//       console.log("Actors: " + pbody.Actors);
-//     }
-//   });
+  request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
 
-// } 
-if (process.argv[2] === "do-what-it-says") {
+    if (!error && response.statusCode === 200) {
+      var pbody = JSON.parse(body)
 
-  
-
-
-
-
-
+      console.log("Title: " + pbody.Title);
+      console.log("Year: " + pbody.Year);
+      console.log("IMBD Rating: " + pbody.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + pbody.Ratings[1].Value);
+      console.log("Country: " + pbody.Country);
+      console.log("Language: " + pbody.Language);
+      console.log("Plot: " + pbody.Plot);
+      console.log("Actors: " + pbody.Actors);
+    }
+  });
 }
